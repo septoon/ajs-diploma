361c6b8d2a43a3a581ac76b3c580b3a2b224eaac
septoon <tigran.darchinyan@gmail.com>
2019-04-17 13:25:05 +0300

    Update GameController.js

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,5 +1,28 @@
+/* eslint-disable no-console */
 import themes from './themes';
-import arrCharacters from './characters/arrCharacters';
+import { usTeam, enTeam } from './characters/arrCharacters'
+import Team from './Team'
+
+// const user = new Team(usTeam);
+// console.log(user);
+// console.log(user.side('user'));
+
+// const enemy = new Team(enTeam);
+// console.log(enemy);
+// console.log(enemy.side('enemy'));
+
+const generateUser = new Team(usTeam, 1, 2);
+console.log(generateUser);
+const generateEnemy = new Team(enTeam, 1, 2);
+console.log(generateEnemy)
+
+const user = generateUser.side('user');
+console.log(user);
+const enemy = generateEnemy.side('enemy');
+console.log(enemy);
+
+
+const allTeam = [user, enemy];
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -11,14 +34,15 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
-    this.gamePlay.redrawPositions(arrCharacters);
+    this.gamePlay.redrawPositions(allTeam);
+
   }
 
-  showSpecific() {
-    this.gamePlay.addCellEnterListner(this.onCellEnter);
-    this.gamePlay.addCellLeaveListner(this.onCellLeave);
-    this.gamePlay.addCellClickListner(this.onCellClick);
-  }
+  // showSpecific() {
+  //   this.gamePlay.addCellEnterListner(this.onCellEnter);
+  //   this.gamePlay.addCellLeaveListner(this.onCellLeave);
+  //   this.gamePlay.addCellClickListner(this.onCellClick);
+  // }
 
   onCellClick(index) {
     // TODO: react to click
