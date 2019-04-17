c589fd78a9600106861f912d0d53b17a6cb065f0
septoon <tigran.darchinyan@gmail.com>
2019-04-17 13:25:00 +0300

    Update Team.js

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,31 +1,37 @@
 import PositionedCharacter from './PositionedCharacter';
+import { generateTeam } from './generators';
 
 const userIndex = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
 const enemyIndex = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
 
 const getUserIndex = () => {
-  const result = [];
   const userRandom = Math.floor(Math.random() * userIndex.length);
-  result.push(userRandom);
+  const result = userIndex[userRandom];
   return result;
 };
 
 const getEnemyIndex = () => {
-  const result = [];
   const enemyRandom = Math.floor(Math.random() * enemyIndex.length);
-  result.push(enemyRandom);
+  const result = enemyIndex[enemyRandom];
   return result;
 };
 
 export default class Team {
-  constructor(team) {
+  constructor(team, level, count) {
     this.team = team;
+    this.level = level;
+    this.count = count;
   }
 
-  character(which) {
-    const userPosition = new PositionedCharacter(this.team, getUserIndex());
-    const enemyPosition = new PositionedCharacter(this.team, getEnemyIndex());
-    if (which === 'user') return userPosition;
-    if (which === 'enemy') return enemyPosition;
+  side(which) {
+    const total = [];
+    const randomTeam = Math.floor(Math.random() * this.team.length);
+    const totalTeam = this.team[randomTeam];
+    const end = generateTeam(totalTeam, this.level, this.count)
+    const userPosition = new PositionedCharacter(end, getUserIndex());
+    const enemyPosition = new PositionedCharacter(end, getEnemyIndex());
+    if (which === 'enemy') total.push(enemyPosition);
+    if (which === 'user') total.push(userPosition);
+    return total;
   }
 }
