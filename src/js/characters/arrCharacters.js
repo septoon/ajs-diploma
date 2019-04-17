78763bc8022f4da7f6094496fbed017966ccec32
septoon <tigran.darchinyan@gmail.com>
2019-04-17 13:22:00 +0300

    userTeam & enemyTeam

--- src/js/characters/arrCharacters.js
+++ src/js/characters/arrCharacters.js
@@ -5,5 +5,28 @@ import Magician from './Magician';
 import Undead from './Undead';
 import Vampire from './Vampire';
 
-const arrCharacters = [Bowman, Swordsman, Daemon, Magician, Undead, Vampire];
+const arrCharacters = [Bowman, Swordsman, Magician, Daemon, Undead, Vampire];
+
+export const usTeam = arrCharacters.slice(0, 3);
+// const randomUser = Math.floor(Math.random() * usTeam.length);
+// const totalUser = usTeam[randomUser];
+
+export const enTeam = arrCharacters.slice(3, 6);
+// const randomEnemy = Math.floor(Math.random() * enTeam.length);
+// const totalEnemy = enTeam[randomEnemy];
+
+// export function getRandomUser () {
+//   const usTeam = arrCharacters.slice(0, 3);
+//   const randomUser = Math.floor(Math.random() * usTeam.length);
+//   const totalUser = usTeam[randomUser];
+//   return totalUser;
+// }
+
+// export function getRandomEnemy () {
+//   const enTeam = arrCharacters.slice(3, 6);
+//   const randomEnemy = Math.floor(Math.random() * enTeam.length);
+//   const totalEnemy = enTeam[randomEnemy];
+//   return totalEnemy;
+// }
+
 export default arrCharacters;
