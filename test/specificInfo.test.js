55ead7a81f3fb68f683a55914c81d0089971348f
septoon <tigran.darchinyan@gmail.com>
2019-04-21 22:23:16 +0300

    Update specificInfo.test.js

--- test/specificInfo.test.js
+++ test/specificInfo.test.js
@@ -1,5 +1,10 @@
-import GameController from '../js/GameController';
+import icons from '../src/js/iconsInfo';
+import Bowman from '../src/js/characters/Bowman';
 
 test('showSpecific', () => {
+  const bowman = new Bowman(1);
+  const received = `${icons.level}${bowman.level} ${icons.attack}${bowman.attack} ${icons.defence}${bowman.defence} ${icons.health}${bowman.health}`;
+  const expected = '🎖1 ⚔25 🛡25 ❤50';
 
+  expect(received).toBe(expected);
 });
